96f4cb5d95f5dd8fc7d1326cf0abcf97e4604e15
Abriham Atinkut <abrihamatinkut@gmail.com>
2026-01-19 18:06:26 +0300

    auto close in menu when items clicked in mobile

--- JS/script.js
+++ JS/script.js
@@ -74,6 +74,16 @@ document.addEventListener("DOMContentLoaded", function () {
       );
     }
   });
+  // auto close when items clicked in mobile
+  document.querySelectorAll(".navbar-nav .nav-link").forEach((link) => {
+    link.addEventListener("click", () => {
+      const navbarCollapse = document.querySelector(".navbar-collapse");
+      const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false,
+      });
+      bsCollapse.hide();
+    });
+  });
 });
 
 // ---------------------------------
